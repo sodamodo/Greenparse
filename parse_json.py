eaaeb8c81a66218b0a2b784d3d1e5c0a1c1faf43
Zack Noah <lamphead@gmail.com>
2018-11-04 12:06:27 -0800

    started parsing through json object of test json files

--- parse_json.py
+++ parse_json.py
@@ -1,6 +1,26 @@
 import json
+filepath = "resume_json/13582.json"
+# readfile = open(filepath, "r")
+# lines = readfile.readlines()
+# readfile.close()
+#
+# writefile = open(filepath, "w")
+# writefile.writelines([item for item in lines[:-1]])
+# writefile.close()
 
-file = open("resume_json/13582.json", "r")
-
-json_object = json.loads(file.read())
+json_file = open(filepath, "r")
+json_object = json.loads(json_file.read())
 print(json_object)
+
+
+first_name = json_object["Resume"]["StructuredXMLResume"]["ContactInfo"]["PersonName"]["GivenName"]
+midle_name =  json_object["Resume"]["StructuredXMLResume"]["ContactInfo"]["PersonName"]["MiddleName"]
+last_name = json_object["Resume"]["StructuredXMLResume"]["ContactInfo"]["PersonName"]["FamilyName"]
+country =  json_object["Resume"]["StructuredXMLResume"]["ContactMethod"]["PostalAddress"]["CountryCode"]
+postal_code = json_object["Resume"]["StructuredXMLResume"]["ContactMethod"]["PostalAddress"]["PostalCode"]
+state = json_object["Resume"]["StructuredXMLResume"]["ContactMethod"]["PostalAddress"]["Region"]
+city = json_object["Resume"]["StructuredXMLResume"]["ContactMethod"]["PostalAddress"]["Municipality"]
+address = json_object["Resume"]["StructuredXMLResume"]["ContactMethod"]["PostalAddress"]["DeliveryAddress"]["AddressLine"]
+
+
+print(first_name, midle_name, last_name, country, postal_code)
